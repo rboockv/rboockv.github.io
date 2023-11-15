8b8faed377ce4872b3f9ef38fd7aac957fca8f4b
rboockv <rudyboockv@gmail.com>
2023-11-15 08:59:44 -0300

    modified default template design

---
+++ src/routes/root.jsx
@@ -0,0 +1,32 @@
+import { useCallback  } from 'react';
+import Particles from "react-particles";
+import { loadFull } from "tsparticles";
+import linkedinLogo from '../assets/LI-In-Bug.png'
+
+function Root() {
+    const particlesInit = useCallback(async engine => {
+        await loadFull(engine);
+    }, []);
+
+    const particlesLoaded = useCallback(() => {}, []);
+
+    return (
+      <>
+      <div className='main-container'>
+        <div className='particles-container'>
+          <Particles id="tsparticles" url="src/assets/particles.json" init={particlesInit} loaded={particlesLoaded} />
+        </div>
+        <div className='info-container'>
+          <h1>Sitio web en mantenimiento</h1>
+          <p>Actualmente se está trabajando en el sitio web.</p>
+          <a href="https://www.linkedin.com/in/rudy-boock-vito/">
+            <img src={linkedinLogo} className="linkedin-logo" alt="Linkedin logo" />
+            /rboockv
+          </a>
+        </div>
+      </div>
+      </>
+    );
+  }
+
+  export default Root
